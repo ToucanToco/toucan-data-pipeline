1f9e1819683539667a210b421948b59cf8a475a7
David Nowinsky <david@nowinsky.net>
2022-01-03 17:22:39 +0100

    Install Cytoscape and use it on an empty shell

--- src/components/AppDataPipeline.vue
+++ src/components/AppDataPipeline.vue
@@ -1,19 +1,41 @@
 <template>
-  <div>App data pipeline</div>
+  <div class="app-data-pipeline">
+    <h1>App data pipeline</h1>
+    <Cytoscape class="app-data-pipeline__viz"></Cytoscape>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
+import Cytoscape from "@/components/Cytoscape.vue";
 
 export default Vue.extend({
-  name: 'AppDataPipeline',
+  name: "AppDataPipeline",
+
+  components: {
+    Cytoscape,
+  },
 
   props: {
     pipeline: {
-      type: Object
-    }
-  }
+      type: Object,
+    },
+  },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.app-data-pipeline {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.app-data-pipeline__viz {
+  flex: 1;
+}
+</style>
---
+++ src/components/Cytoscape.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cytoscape-viz"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import cytoscape from "cytoscape";
+
+export default Vue.extend({
+  name: "Cytoscape",
+
+  mounted() {
+    var cy = cytoscape({
+      container: this.$el,
+    });
+  },
+});
+</script>
+
+<style scoped></style>
