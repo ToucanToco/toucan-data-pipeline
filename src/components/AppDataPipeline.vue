d518bda3fec6cf7a4f48a91f1ce391b76172fc7d
David Nowinsky <david@nowinsky.net>
2022-01-03 16:52:45 +0100

    Start AppDataPipeline component

---
+++ src/components/AppDataPipeline.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>App data pipeline</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppDataPipeline',
+
+  props: {
+    pipeline: {
+      type: Object
+    }
+  }
+});
+</script>
+
+<style scoped></style>
